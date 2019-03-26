9fa858b42819776e6b9b5ac8010005df4107ee5e
Puja Grover <puja_grover@yahoo.com>
2019-03-25 20:11:00 -0500

    Week9HwStudentMinMax:Tested,Commit,Sync,Push

--- StudentMinMax/Program.cs
+++ StudentMinMax/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+//using System.Collections;
+using System.Collections.Generic;
+//using System.IO;
+//using StudentMinMax;
 
 namespace StudentMinMax
 {
@@ -6,12 +10,53 @@ namespace StudentMinMax
     {
         public static void Main()
         {
+            List<Student> students = new List<Student>();
+            Student studentNameAndScores = new Student();
+            students = studentNameAndScores.GetStudentScoresFromFileStudentData();
             foreach (var student in students)
             {
                 Console.WriteLine($"{student.Name} Min: {student.GetMinimumScore()} Max: {student.GetMaximumScore()}");
             }
-
             Console.ReadLine();
+
         }
+
+        //// get data from file
+        //private List<Student> GetStudentScoresFromFileStudentData()
+        //{
+        //    //Declaring a List that will contain each student detail stored line by line
+        //    List<Student> students = new List<Student>();
+        //    Student student = new Student();
+        //    //Reading the text file line by line
+        //    //Storing each student detail in Student type student
+        //    foreach (string line in File.ReadLines(@"Solution Items\studentdata.txt"))
+        //    {
+        //        string[] eachStudentDetail = line.Split(" ");
+
+        //        for(int i = 0; i < eachStudentDetail.Length; i++)
+        //        {
+        //            if (i == 0)
+        //                student.Name = eachStudentDetail[0];
+        //            //else (i > 0)
+        //            //{
+        //            //    ArrayList tempScore = new ArrayList();
+        //            //    tempScore.Add[i];
+
+        //            //    student.Scores = 
+        //            //}
+                        
+
+        //        }
+                
+        //       // string[] tempScoreStringArray = eachStudentDetail[1; eachStudentDetail.Length];
+        //        Console.WriteLine(student);
+        //    }
+        //    students.Add(student);
+        //    Console.WriteLine(students);
+        //    Console.ReadLine();
+
+
+        //    return students;
+        
     }
 }
--- StudentMinMax/Student.cs
+++ StudentMinMax/Student.cs
@@ -1,4 +1,9 @@
-﻿namespace StudentMinMax
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StudentMinMax
 {
     public class Student
     {
@@ -8,10 +13,67 @@
 
         public int? GetMaximumScore()
         {
+            int maxScore = 0;
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                if (Scores[i] > maxScore)
+                    maxScore = Scores[i];
+            }            
+            return maxScore;
         }
 
         public int? GetMinimumScore()
         {
+            int minScore = 0;
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                if (i == 0)
+                minScore = Scores[0];
+                else if(Scores[i] < minScore)
+                {        
+                    minScore = Scores[i];
+                }
+
+                 
+            }
+            return minScore;
         }
+        // add data from file into the list line by line
+        public List<Student> GetStudentScoresFromFileStudentData()
+        {
+            //Declaring a List that will contain each student detail stored line by line
+            List<Student> students = new List<Student>();
+            foreach (string line in File.ReadLines(@"studentdata.txt"))
+            {
+                Student student = CreateStudentRecord(line);
+                students.Add(student);
+
+            }
+            return students;
+        }
+
+        public Student CreateStudentRecord(string line)
+        {
+            Student student = new Student();
+
+            string[] eachStudentDetail = line.Split(" ");
+
+            string[] tempScore = new string[eachStudentDetail.Length - 1];
+            for (int i = 0; i < eachStudentDetail.Length; i++)
+            {
+                if (i == 0)
+                    student.Name = eachStudentDetail[0];
+                else 
+                {     
+                    tempScore[i-1] = eachStudentDetail[i];
+                   
+                }
+
+            }
+            student.Scores = Array.ConvertAll(tempScore, int.Parse);
+
+            return student;
+        }
+
     }
 }
